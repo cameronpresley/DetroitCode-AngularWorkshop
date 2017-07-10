e9af4b9e39929c6c8b66c82ec09753a25894b87b
Cameron Presley <cameron@thesoftwarementor.com>
2017-07-10 11:16:58 -0400

    Template Based Forms - Chapter 5 (#2)
    * Created a login component.
    
    * Added /login to the routes.
    
    * Updated the Login.html to have input elements.
    
    * Added FormsModule, updated Login so that when the submit button is clicked, it passes the email/password to the component.
    
    * Added the authorization service component.
    
    * Changed error handling to display an error div instead.
    
    * Added required validation checking for email and password fields.
    
    * Added logic to show a different error message if the email address is not valid.
    
    * Added logic to disable the button if the form is not valid.
    
    * Added the signup component, added link between navigating to signup vs login.

---
+++ src/app/shared/services/auth.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { Http, Response, RequestOptions } from '@angular/http';
+import { Observable } from 'rxjs/Rx';
+
+@Injectable()
+export class AuthService {
+  private options = new RequestOptions({withCredentials:true});
+
+  constructor(private http:Http) { }
+
+  login(email: string, password: string): Observable<boolean>{
+    let loginInfo = {"email":email, "password":password};
+    return this.http.put("https://dj-sails-todo.azurewebsites.net/user/login", loginInfo, this.options)
+                .do((res: Response) => {
+                  if (res){
+                    return Observable.of(true);
+                  }
+                  return Observable.of(false);
+                })
+                .catch(error => {
+                  console.log('login error', error);
+                  return Observable.of(false);
+                });
+  }
+  signup(email:string, password:string): Observable<boolean>{
+    let loginInfo = {"email": email, "password":password};
+    return this.http.post("https://dj-sails-todo.azurewebsites.net/user/", loginInfo, this.options)
+      .do((res: Response) => {
+        if (res){
+          return Observable.of(true);
+        }
+        return Observable.of(false);
+        
+      })
+      .catch(error => {
+        console.log('signup error', error);
+        return Observable.of(false);
+      });
+  }
+}
